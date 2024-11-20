6aca6215eb2d525eff6b0316a9cb7f9b44c1e9e3
Müller <mrmiller94@gmail.com>
2024-11-20 19:49:44 -0300

    Algumas alterações

---
+++ whois_consulta.py
@@ -0,0 +1,12 @@
+import whois  # Importa o módulo python-whois
+
+dominio = input('Digite o domínio: ')
+
+try:
+    # Consulta o whois do domínio
+    resultado = whois.whois(dominio)
+
+    # Mostra o resultado
+    print(resultado)
+except Exception as e:
+    print(f'Ocorreu um erro: {e}')
